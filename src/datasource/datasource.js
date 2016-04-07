f5dc352c0f0ed8a68f527d66d72c7903cf9eb378
Mitsuhiro Tanda <mitsuhiro.tanda@gmail.com>
2016-04-08 01:10:55 +0900

    tmp

--- src/datasource/datasource.js
+++ src/datasource/datasource.js
@@ -16,21 +16,11 @@ export class PrometheusPullDatasource {
     return this.backendSrv.datasourceRequest(options);
   }
 
-  getTasks() {
-    return this.request({method: 'get', url: '/v1/tasks'}).then(res => {
-      if (!res.data || !res.data.body || !res.data.body.ScheduledTasks) {
-        return [];
-      }
-
-      return res.data.body.ScheduledTasks;
-    });
-  }
-
   emptyResult() {
     return Promise.resolve({data: []});
   }
 
-  getMetrics() {
+  getMetricEntries() {
     return this.request({method: 'get', url: '/metrics'}).then(res => {
       return res.data.body.split(/\n/).filter(function(l) {
         return l.indexOf('#') !== 0;
@@ -46,63 +36,6 @@ export class PrometheusPullDatasource {
     });
   }
 
-  createTask(target) {
-    if (target.metrics.length === 0) {
-      return Promise.reject("No metrics selected for task");
-    }
-
-    var task = {
-      version: 1,
-      name: target.taskName,
-      start: true,
-      schedule: {
-        type: 'simple',
-        interval: target.interval,
-      },
-      workflow: {
-        collect: {
-        }
-      },
-      publish: []
-    };
-
-    task.workflow.collect.metrics = target.metrics.reduce((memo, metric) => {
-      memo[metric.namespace] = {};
-      return memo;
-    }, {});
-
-    console.log('creating task', task);
-    return this.request({method: 'post', url: '/v1/tasks', data: task}).then(res => {
-      console.log('created task', res);
-      return res.data.body;
-    });
-  }
-
-  // getTaskId(target) {
-  //   if (!target) {
-  //     return Promise.resolve(null);
-  //   }
-  //
-  //   switch (target.mode) {
-  //     case 'Watch Task': {
-  //       if (!target.taskId) {
-  //         return Promise.resolve(null);
-  //       }
-  //
-  //       return this.getTask(target.taskId).then(task => {
-  //         return task.id;
-  //       });
-  //     }
-  //     case  'Define Task': {
-  //       if (target.taskId) {
-  //         return this.getTask(target.taskId).then(task => {
-  //           return task.id;
-  //         });
-  //       }
-  //     }
-  //   }
-  // }
-
   query(options) {
     var handler = this.streamHandlers[options.panelId];
     if (handler) {
@@ -113,36 +46,6 @@ export class PrometheusPullDatasource {
     handler.start();
 
     return Promise.resolve(handler);
-
-    // if (this.runningQuery) {
-    //   return this.runningQuery;
-    // }
-    //
-    // var target = options.targets[0];
-    // this.runningQuery = this.getTaskId(target).then(taskId => {
-    //
-    //   if (!taskId) {
-    //     return this.emptyResult();
-    //   }
-    //
-    //   var watchUrl = this.url + '/v1/tasks/' + taskId + '/watch';
-    //   this.observable = Observable.create(observer => {
-    //
-    //     var handler = new StreamHandler();
-    //     handler.start(observer, watchUrl);
-    //
-    //     return () => {
-    //       handler.close();
-    //       this.observable = null;
-    //     };
-    //   });
-    //
-    //   return this.observable;
-    // }).finally(() => {
-    //   this.runningQuery = null;
-    // });
-    //
-    // return this.runningQuery;
   }
 
   getMetrics() {
@@ -150,7 +53,7 @@ export class PrometheusPullDatasource {
       return Promise.resolve(this.metricsCache);
     }
 
-    return this.request({method: 'get', url: '/v1/metrics'}).then(res => {
+    return this.request({method: 'get', url: '/metrics'}).then(res => {
       if (!res.data || !res.data.body || !res.data.body) {
         return [];
       }
@@ -162,18 +65,5 @@ export class PrometheusPullDatasource {
       return this.metricsCache;
     });
   }
-
-  deleteTask(taskId) {
-    return this.request({method: 'delete', url: '/v1/tasks/' + taskId});
-  }
-
-  startTask(taskId) {
-    return this.request({method: 'put', url: '/v1/tasks/' + taskId + '/start'});
-  }
-
-  stopTask(taskId) {
-    return this.request({method: 'put', url: '/v1/tasks/' + taskId + '/stop'});
-  }
-
 }
 
